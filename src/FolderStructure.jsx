e521093e25bc2029a8a5a6b9ccb9d7cba7c46717
Sai Ankitha Katravulapalli <ankitha@Kratos.local>
2023-12-28 14:30:56 +0530

    Assignment for folder structure

---
+++ src/FolderStructure.jsx
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from 'react';
+import folderData from './folderData.json';
+// Function to return JSON as a promise
+const getFolderData = () => {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve(folderData);
+    }, 2000); // 2 secs delay
+  });
+};
+
+// Component to display folders and files
+const FolderComponent = ({ data }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const isFolder = data.type === 'dir';
+
+  const toggleFolder = () => isFolder && setIsOpen(!isOpen);
+
+  return (
+    <div style={{ marginLeft: 20 }}>
+      <div onClick={toggleFolder}>
+        {isFolder && (isOpen ? '[-] ' : '[+] ')}
+        {data.file}
+      </div>
+      {isOpen && isFolder && data.contents.map((item, index) => (
+        <FolderComponent key={index} data={item} />
+      ))}
+    </div>
+  );
+};
+
+const FolderStructure = () => {
+  const [folderData, setFolderData] = useState(null);
+
+  useEffect(() => {
+    getFolderData().then(data => {
+      setFolderData(data);
+    });
+  }, []);
+
+  return (
+    <div>
+      {folderData ? folderData.contents.map((item, index) => (
+        <FolderComponent key={index} data={item} />
+      )) : <div>Loading...</div>}
+    </div>
+  );
+};
+
+export default FolderStructure;
